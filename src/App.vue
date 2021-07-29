7475349b233fbb8ec8ef5f30b5339a937c83728c
ErrolLin <errolriccolin@foxmail.com>
2021-07-29 09:38:30 +0800

    Add navigation drawers

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Best Todo Ever!
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -14,18 +34,22 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { title: 'Todo', icon: 'mdi-view-dashboard' },
+      { title: 'About', icon: 'mdi-help-box' },
+    ],
+    right: null,
   }),
 };
 </script>
