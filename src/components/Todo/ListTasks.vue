9a5168df2618138104983fdda4b72eacd5b9bb43
ErrolLin <errolriccolin@foxmail.com>
2021-08-05 10:17:56 +0800

    Divide Todo page into some components

---
+++ src/components/Todo/ListTasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-list class="pt-0" flat>
+    <task v-for="task in tasks" :key="task.id" :task="task"></task>
+  </v-list>
+</template>
+
+<script>
+export default {
+  components: {
+    task: require('./Task.vue').default,
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
+  state() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up',
+          done: false,
+        },
+        {
+          id: 2,
+          title: 'Get bananas',
+          done: false,
+        },
+        {
+          id: 3,
+          title: 'Eat bananas',
+          done: false,
+        },
+      ],
+    };
   },
+  getters: {},
   mutations: {
+    addTask(state,payload) {
+      let newTask = {
+        id: Date.now(),
+        title: payload,
+        done: false,
+      };
+      state.tasks.push(newTask);
+    },
+    doneTask(state, payload) {
+      let id = payload;
+      let task = state.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(state, payload) {
+      let id = payload;
+      state.tasks = state.tasks.filter((task) => task.id !== id);
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
